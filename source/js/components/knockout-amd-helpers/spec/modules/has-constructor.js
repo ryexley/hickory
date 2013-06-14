1d9260c23e9f0d1ff9319516320d9a25c437090c
Bob Yexley <bob@yexley.net>
2013-06-13 20:31:13 -0400

    Major project reorganization in an effort to structure it as an official open source module

--- source/js/components/knockout-amd-helpers/spec/modules/has-constructor.js
+++
@@ -1,7 +0,0 @@
-define(["knockout"], function(ko) {
-    return function(data) {
-        data = data || {};
-        this.first = ko.observable(data.first || "Ted");
-        this.last = ko.observable(data.last || "Jones");
-    };
-});
